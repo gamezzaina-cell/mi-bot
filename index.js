92de24de3724780500bfe1fae3f2fe695addccd4
gamezzaina-cell <gamezzaina@gmail.com>
2026-04-21 20:00:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,40 @@ client.on('messageCreate', async message => {
   });
 });
 
+client.on('messageCreate', async message => {
+  if (message.content === '!info10m') {
+
+    message.channel.send(`# **¿Quieres darte a conocer en Valorant? :dart:**
+
+***Únete a nuestras 10mans y demuestra de lo que eres capaz.
+
+¿Eres un equipo en busca de jugadores? Observa talento en acción y encuentra a tu próximo fichaje.
+¿Eres jugador y buscas equipo? Este es tu momento para brillar y mostrar tu nivel.
+
+Compite, destaca y haz que te vean.
+
+:point_right: Entra ahora y empieza a jugar con los mejores.
+:point_right: https://forms.gle/RDXhhJQXN2CR4S9K6 ***
+
+
+
+# **Do you want to make a name for yourself in Valorant? :dart:**
+***
+Join our 10mans and prove what you're capable of.
+
+Are you a team looking for players? Watch talent in action and find your next recruit.
+Are you a player looking for a team? This is your moment to shine and showcase your level.
+
+Compete, stand out, and get noticed.
+
+:point_right: Join now and start playing with the best.
+
+:point_right: https://forms.gle/RDXhhJQXN2CR4S9K6
+***
+
+||@everyone ||`);
+  }
+});
 
 // 🔐 LOGIN
 client.login(process.env.TOKEN);
